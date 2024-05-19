dc7a00fb6c2a343f9d9d1e38b4fd5cc90b0124cf
JuanDLara <juandavid.lara.20@gmail.com>
2024-05-18 20:14:06 -0500

    configuracion Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Azul-oscuro': '#0081A7',
+        'Azul-claro': '#00AFB9',
+        'Beige': '#EEF4ED',
+        'Salmon': '#EDDEA4',
+        'Rojo': '#F7A072',
+      }
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
 }
 
