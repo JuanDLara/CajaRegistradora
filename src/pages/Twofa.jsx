ff8415132a0a012237881dbceea1b37670d21fa0
celura <u20212199910@usco.edu.co>
2024-05-19 01:16:00 -0500

    se termino el header, footer, login y twofa

--- src/pages/Twofa.jsx
+++ src/pages/Twofa.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { TbPasswordMobilePhone } from "react-icons/tb";
+
 
 const Twofa = () => {
+  const [code, setCode] = useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    if (/^\d*$/.test(value) && value.length <= 4) {
+      setCode(value);
+    }
+  };
   return (
-    <div>Twofa</div>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
+      <div className="flex justify-center space-x-2">
+        <TbPasswordMobilePhone className="text-xl size-12" />
+        <h2 className=" text-2xl font-bold text-gray-900">
+          Insert your telegram code
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" action="#" method="POST">
+          <div className="flex items-center justify-center">
+            <input
+              type="text"
+              value={code}
+              onChange={handleChange}
+              maxLength="4"
+              className="border border-gray-300 rounded-md p-2 w-20 text-center text-2xl"
+              placeholder="0000"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-emerald-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-emerald-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-900"
+            >
+              Send
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
